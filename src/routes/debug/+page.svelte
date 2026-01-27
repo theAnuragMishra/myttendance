865e81794c3537b003448a8450042c87ff923298
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2026-01-27 21:45:32 +0530

    add a debug page

---
+++ src/routes/debug/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { onMount } from 'svelte';
+	let persisted = $state(false);
+	onMount(async () => {
+		persisted = await navigator.storage.persisted();
+	});
+</script>
+
+<p>Persistence permission is {persisted}</p>
